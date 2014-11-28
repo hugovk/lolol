aa581c5f6fce9e2db08b75d2781d63adddc98951
hugovk <hugovk@users.noreply.github.com>
2014-11-28 14:22:33 +0200

    Lol!

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 lolol
 =====
 
-Replace every "lo" in Project Gutenberg with a "lol". For NaNoGenMo 2014.
+*or*
+
+lolol
+-----
+
+*or*
+
+The Big Jolly Book of Humorous Observations
+-------------------------------------------
+
+In the [Project Gutenberg CD corpus of 600 books](http://www.gutenberg.org/wiki/Gutenberg:The_CD_and_DVD_Project), find every English sentence containing the word **lo** -- but also include the next sentence because the parser sometimes breaks things like "lo! and behold" into two.
+
+Then replace each **lo** with **lol**.
+
+    gutengrep.py -i "\blo\b" "*.txt" --cache --sort --correct -o /tmp/lolol.txt --andnext --language en
+    lolol.py lolol-sort.txt  > lolol.md
+    multimarkdown lolol.md > lolol.html && lolol.html
+
+Uses [gutengrep.py](https://github.com/hugovk/gutengrep).
+
+Then print to PDF using Chrome. Big thanks to [@moonmilk for the CSS](https://github.com/moonmilk/nanogenmo2014):
+
+> Learning how to use print css to make the PDF look more like a cheap paperback and less like the printout from a web browser. css @page object and widows, orphans, and page-break-before properties allow some simple but effective page formatting. I also added a slightly tacky free google font to further distance the resulting look from default browser printouts.
---
+++ lolol.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+Replace every "lo" in Project Gutenberg with a "lol".
+Usage:
+gutengrep.py -i "\blo\b" "*.txt" --cache --sort --correct -o /tmp/lolol.txt
+    --andnext --language en
+lolol.py lolol-sort.txt  > lolol.md
+multimarkdown lolol.md > lolol.html && lolol.html
+"""
+from __future__ import print_function, unicode_literals
+import fileinput
+import re
+
+CHARS = "\`*_{}[]()>#+-.!$"
+
+
+def front():
+    front_matter = [
+        "Title:          lolol\n"
+        "CSS:            lolol.css\n"
+        "HTML Header:    <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,700,400italic'>",
+        "",
+        "lolol",
+        "=====",
+        "*<center>or</center>*",
+        'ol the <span class="lol">lols</span>',
+        "-----------",
+        "*<center>or</center>*",
+        "The Big Jolly Book of Humorous Observations",
+        "-----------------------------------------------------",
+        "",
+        '<p class="author">lolol.py</p>',
+        "",
+        '<p class="auth-desc">for NaNoGenMo 2014</p>',
+        "",
+        '<p class="source">source code at https://github.com/hugovk/lolol</p>',
+        "",
+    ]
+    print("\n".join(front_matter))
+
+
+if __name__ == '__main__':
+    front()
+
+    print("### Chapter 1\n")
+    for line in fileinput.input():
+    # for i, line in enumerate(fileinput.input()):
+        # if i % 120 == 0:
+            # print("### Chapter " + str(i+1) + "\n")
+        line = line.decode('utf-8-sig').rstrip()  # No BOM
+        for c in CHARS:
+            if c in line:
+                line = line.replace(c, '\\' + c)
+
+        # lo -> *lol*
+        line = re.sub(r"\blo\b", '<span class="lol">lol</span>', line)
+        line = re.sub(r'\bLO\b', '<span class="lol">LOL</span>', line)
+        line = re.sub(r'\bLo\b', '<span class="lol">Lol</span>', line)
+
+        print(line.encode('utf-8'))
+
+# End of file
